c221fd99ee80a70ff505aee010159f795f599c1c
sbejaoui <souheil.bejaoui@acsone.eu>
2019-09-09 11:42:40 +0200

    [12.0][ADD] - Add contract_brand module

---
+++ contract_brand/models/contract.py
@@ -0,0 +1,17 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class ContractContract(models.Model):
+
+    _name = 'contract.contract'
+    _inherit = ['contract.contract', 'res.brand.mixin']
+
+    @api.multi
+    def _prepare_invoice(self, date_invoice, journal=None):
+        self.ensure_one()
+        values = super()._prepare_invoice(date_invoice, journal)
+        values["brand_id"] = self.brand_id
+        return values
