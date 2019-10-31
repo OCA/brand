5f246c56244f123339cfd6c00d0f4cbc44372e8c
sbejaoui <souheil.bejaoui@acsone.eu>
2019-10-31 18:04:29 +0100

    [12.0][REF] - move brand model definition to brand module

--- partner_brand/__manifest__.py
+++ partner_brand/__manifest__.py
@@ -1,5 +1,4 @@
 # Copyright (C) 2019 Open Source Integrators
-# Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -9,15 +8,13 @@
     "category": "Sales Management",
     "website": "https://github.com/OCA/sale-workflow",
     "author": "Open Source Integrators,"
-              "ACSONE SA/NV,"
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": [
         "contacts",
+        "brand",
     ],
     "data": [
-        'security/res_brand.xml',
-        'views/res_brand.xml',
         "views/report_template.xml",
     ],
     "installable": True,
--- partner_brand/readme/USAGE.rst
+++
@@ -1,4 +0,0 @@
-To use this module, you need to:
-
-#. Go to Settings > Users & Companies > Brands
-#. Add a new brand
