c852e02cece20f0fe9df4243d34fff788f1d20d7
sbejaoui <souheil.bejaoui@acsone.eu>
2019-11-08 14:17:02 +0100

    [12.0][IMP] - Add res partner account brand

--- account_brand/__manifest__.py
+++ account_brand/__manifest__.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2019 Open Source Integrators
+# Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -7,7 +8,9 @@
     "version": "12.0.3.0.3",
     "category": "Accounting Management",
     "website": "https://github.com/OCA/account-invoicing",
-    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "author": "Open Source Integrators,"
+              "ACSONE SA/NV,"
+              "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": [
         'account',
@@ -16,6 +19,8 @@
     ],
     "data": [
         "views/account_invoice.xml",
+        "views/res_partner_account_brand.xml",
+        "security/res_partner_account_brand.xml",
     ],
     "installable": True,
     "development_status": "Beta",
