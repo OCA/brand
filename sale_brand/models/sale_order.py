4d757f02ae511d1b57d9822b0e14aa3c8d0267ed
sbejaoui <souheil.bejaoui@acsone.eu>
2019-12-30 15:04:50 +0100

    [IMP] - set the sale order analytic account at brand change

--- sale_brand/models/sale_order.py
+++ sale_brand/models/sale_order.py
@@ -26,6 +26,14 @@ class SaleOrder(models.Model):
             })
         return invoice_vals
 
+    @api.onchange('brand_id')
+    def _onchange_brand_id(self):
+        res = super()._onchange_brand_id()
+        for order in self:
+            if order.state == 'draft' and order.brand_id:
+                order.analytic_account_id = order.brand_id.analytic_account_id
+        return res
+
     @api.onchange('team_id')
     def _onchange_team_id(self):
         self.brand_id = self.team_id.brand_id
