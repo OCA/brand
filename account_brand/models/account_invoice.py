292e9f240a41f38f1a9638ccd072a1134eb73c9b
sbejaoui <souheil.bejaoui@acsone.eu>
2020-02-13 11:40:15 +0100

    [12.0][IMP] - Brand field is editable in draft state for invoice and sale object

--- account_brand/models/account_invoice.py
+++ account_brand/models/account_invoice.py
@@ -1,9 +1,18 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import fields, models
 
 
 class AccountInvoice(models.Model):
     _name = 'account.invoice'
     _inherit = ['account.invoice', 'res.brand.mixin']
+
+    brand_id = fields.Many2one(
+        states={
+            'open': [('readonly', True)],
+            'in_payment': [('readonly', True)],
+            'paid': [('readonly', True)],
+            'cancel': [('readonly', True)],
+        }
+    )
