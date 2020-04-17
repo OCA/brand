4ed1262ab8defeec8e33798a6ac28ff5bfa69876
cubells <vicent@vcubells.net>
2020-04-17 19:45:40 +0200

    [MIG] sale_brand: Migration to 13.0

--- sale_brand/models/sale_order.py
+++ sale_brand/models/sale_order.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
@@ -17,7 +17,6 @@ class SaleOrder(models.Model):
         }
     )
 
-    @api.multi
     def _prepare_invoice(self):
         invoice_vals = {}
         for order in self:
