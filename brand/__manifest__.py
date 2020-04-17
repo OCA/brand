f5c6bb8a1735e213bfaa1d2cfd2ae0b9b08c0993
cubells <vicent@vcubells.net>
2020-04-17 19:47:46 +0200

    [IMP] brand: black, isort, prettier

--- brand/__manifest__.py
+++ brand/__manifest__.py
@@ -2,20 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Brand',
-    'summary': """
+    "name": "Brand",
+    "summary": """
         This is a base addon for brand modules. It adds the brand object and
         its menu and define an abstract model to be inherited from branded
         objects""",
-    'version': '12.0.1.1.1',
-    'license': 'AGPL-3',
-    'author': 'Odoo Community Association (OCA),'
-              'ACSONE SA/NV',
-    'website': 'https://github.com/OCA/brand',
-    'depends': ['base_setup'],
-    'data': [
-        'views/res_config_settings.xml',
-        'security/res_brand.xml',
-        'views/res_brand.xml',
+    "version": "12.0.1.1.1",
+    "license": "AGPL-3",
+    "author": "Odoo Community Association (OCA)," "ACSONE SA/NV",
+    "website": "https://github.com/OCA/brand",
+    "depends": ["base_setup"],
+    "data": [
+        "views/res_config_settings.xml",
+        "security/res_brand.xml",
+        "views/res_brand.xml",
     ],
 }
