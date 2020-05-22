df9cfa432895c20ea939350c6fa1a81a52b842ca
cubells <vicent@vcubells.net>
2020-05-22 16:54:08 +0200

    [MIG] brand_external_report_layout: Migration to 13.0

--- brand_external_report_layout/tests/test_brand_external_report_layout.py
+++ brand_external_report_layout/tests/test_brand_external_report_layout.py
@@ -7,15 +7,15 @@ from odoo.tests.common import TransactionCase
 class TestBrandExternalReportLayout(TransactionCase):
     def setUp(self):
         super(TestBrandExternalReportLayout, self).setUp()
-        self.brand = self.env['res.brand'].create({'name': 'brand'})
+        self.brand = self.env["res.brand"].create({"name": "brand"})
 
     def test_change_report_template(self):
         self.brand.change_report_template()
         self.assertEqual(
             self.env.ref(
-                'brand_external_report_layout.res_brand_document_template_form'
+                "brand_external_report_layout.res_brand_document_template_form"
             ).id,
-            self.brand.change_report_template()['view_id'],
+            self.brand.change_report_template()["view_id"],
         )
 
     def test_get_default_brand_logo(self):
