7f97917dbd7e13ffc1f76712a234fa0ed2ebe5fc
cubells <vicent@vcubells.net>
2020-05-22 16:54:08 +0200

    [MIG] brand_external_report_layout: Migration to 13.0

--- brand_external_report_layout/__manifest__.py
+++ brand_external_report_layout/__manifest__.py
@@ -2,15 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Brand External Report Layout',
-    'summary': """
+    "name": "Brand External Report Layout",
+    "summary": """
         This module allows you to have a different layout by brand for your
         external reports.""",
-    'version': '12.0.1.0.2',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/brand',
-    'depends': ['base', 'brand', 'web'],
-    'data': ['views/res_brand.xml', 'views/report_template.xml'],
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/brand",
+    "depends": ["base", "brand", "web"],
+    "data": ["views/res_brand.xml", "views/report_template.xml"],
+    "maintainers": ["sbejaoui"],
 }
