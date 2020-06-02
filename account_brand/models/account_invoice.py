c65e6db142cdf9b80227f6603e2b5757f6771b9d
sbejaoui <souheil.bejaoui@acsone.eu>
2020-06-02 16:05:19 +0200

    [FIX] - Fix brand onchange

--- account_brand/models/account_invoice.py
+++ account_brand/models/account_invoice.py
@@ -18,46 +18,47 @@ class AccountInvoice(models.Model):
         }
     )
 
-    @api.onchange("partner_id", "company_id", "brand_id")
+    @api.onchange("partner_id", "company_id")
     def _onchange_partner_id(self):
         res = super()._onchange_partner_id()
-        if not self.brand_id:
-            return res
-        partner_account_brand_model = self.env["res.partner.account.brand"]
-        company_id = self.company_id.id
-        partner = (
-            self.partner_id
-            if not company_id
-            else self.partner_id.with_context(force_company=company_id)
-        )
-        invoice_type = self.type or self.env.context.get("type", "out_invoice")
-        if partner:
-            rec_account = (
-                partner_account_brand_model._get_partner_account_by_brand(
+        self._onchange_partner_brand()
+        return res
+
+    @api.onchange("partner_id", "company_id", "brand_id")
+    def _onchange_partner_brand(self):
+        if self.brand_id:
+            pab_model = self.env["res.partner.account.brand"]
+            company_id = self.company_id.id
+            partner = (
+                self.partner_id
+                if not company_id
+                else self.partner_id.with_context(force_company=company_id)
+            )
+            invoice_type = self.type or self.env.context.get(
+                "type", "out_invoice"
+            )
+            if partner:
+                rec_account = pab_model._get_partner_account_by_brand(
                     "receivable", self.brand_id, partner
                 )
-            )
-            rec_account = (
-                rec_account
-                if rec_account
-                else partner.property_account_receivable_id
-            )
-            pay_account = (
-                partner_account_brand_model._get_partner_account_by_brand(
+                rec_account = (
+                    rec_account
+                    if rec_account
+                    else partner.property_account_receivable_id
+                )
+                pay_account = pab_model._get_partner_account_by_brand(
                     "payable", self.brand_id, partner
                 )
-            )
-            pay_account = (
-                pay_account
-                if pay_account
-                else partner.property_account_payable_id
-            )
-            if invoice_type in ("in_invoice", "in_refund"):
-                account_id = pay_account
-            else:
-                account_id = rec_account
-            self.account_id = account_id
-        return res
+                pay_account = (
+                    pay_account
+                    if pay_account
+                    else partner.property_account_payable_id
+                )
+                if invoice_type in ("in_invoice", "in_refund"):
+                    account_id = pay_account
+                else:
+                    account_id = rec_account
+                self.account_id = account_id
 
     @api.onchange('brand_id', 'invoice_line_ids')
     def _onchange_brand_id(self):
