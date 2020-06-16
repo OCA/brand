68dbaaff51c7efc318eb26fb1c067c4b04f9818d
Quentin Groulard <quentin.groulard@acsone.eu>
2020-06-16 11:02:58 +0200

    [FIX] Invoice account_id

--- contract_brand/tests/test_contract.py
+++ contract_brand/tests/test_contract.py
@@ -8,6 +8,27 @@ class TestContract(TestContractBase):
     def setUp(self):
         super(TestContract, self).setUp()
         self.brand_id = self.env["res.brand"].create({"name": "brand"})
+        self.account_receivable_type = self.env.ref(
+            'account.data_account_type_receivable'
+        )
+        self.account_receivable_brand_default = self.env[
+            'account.account'
+        ].create(
+            {
+                'name': 'Receivable Brand Default',
+                'code': 'RCV01',
+                'user_type_id': self.account_receivable_type.id,
+                'reconcile': True,
+            }
+        )
+        self.partner_account_brand = self.env['res.partner.account.brand'].create(
+            {
+                'partner_id': self.contract.partner_id.id,
+                'account_id': self.account_receivable_brand_default.id,
+                'brand_id': self.brand_id.id,
+                'account_type': 'receivable',
+            }
+        )
 
     def test_contract_create_branded_invoice(self):
         """ It should create a branded invoice based on the contract brand
@@ -15,6 +36,7 @@ class TestContract(TestContractBase):
         self.contract.brand_id = self.brand_id
         invoice = self.contract.recurring_create_invoice()
         self.assertEqual(invoice.brand_id, self.brand_id)
+        self.assertEqual(invoice.account_id, self.account_receivable_brand_default)
 
     def test_contract_analytic_account_onchange_brand(self):
         self.brand_id.analytic_account_id = self.env[
