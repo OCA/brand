1f87f7e36916d0d376a285d1f6ebddb8eff0a4bb
Quentin Groulard <quentin.groulard@acsone.eu>
2020-08-11 17:05:55 +0200

    [12.0][FIX] Brand not required on vendor bill

--- account_brand/models/account_invoice.py
+++ account_brand/models/account_invoice.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
@@ -16,3 +16,10 @@ class AccountInvoice(models.Model):
             'cancel': [('readonly', True)],
         }
     )
+
+    @api.constrains('brand_id', 'company_id')
+    def _check_brand_requirement(self):
+        out_invoices = self.filtered(
+            lambda l: l.type in ('out_invoice', 'out_refund')
+        )
+        return super(AccountInvoice, out_invoices)._check_brand_requirement()
