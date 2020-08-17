fe8dadc199d466ac7dd595751ac4ab031b20c5c8
sbejaoui <souheil.bejaoui@acsone.eu>
2020-08-17 11:23:03 +0200

    Use more permissive license AGPL3 → LGPL3
    This change will allow to have OE branded objects.

--- brand/__manifest__.py
+++ brand/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2019 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
 {
     'name': 'Brand',
@@ -8,7 +8,7 @@
         its menu and define an abstract model to be inherited from branded
         objects""",
     'version': '12.0.1.1.2',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'author': 'Odoo Community Association (OCA),'
               'ACSONE SA/NV',
     'website': 'https://github.com/OCA/brand',
