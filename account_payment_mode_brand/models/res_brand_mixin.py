0ab3731464146c50f0c8ea5e853dd46ef48fe9ac
sbejaoui <souheil.bejaoui@acsone.eu>
2020-09-15 11:08:52 +0200

    [IMP] - use computed field for allowed_payment_mode

--- account_payment_mode_brand/models/res_brand_mixin.py
+++ account_payment_mode_brand/models/res_brand_mixin.py
@@ -9,13 +9,17 @@ class ResBrandMixin(models.AbstractModel):
     _inherit = "res.brand.mixin"
 
     allowed_payment_mode_ids = fields.Many2many(
-        related="brand_id.allowed_payment_mode_ids"
+        comodel_name="account.payment.mode",
+        compute="_compute_allowed_payment_mode_ids",
     )
 
-    @api.onchange("brand_id")
-    def onchange_brand_allowed_payment_mode(self):
-        self.ensure_one()
-        domain = []
-        if self.brand_id and self.allowed_payment_mode_ids:
-            domain = [("id", "in", self.allowed_payment_mode_ids.ids)]
-        return {"domain": {"payment_mode_id": domain}}
+    @api.depends('brand_id')
+    def _compute_allowed_payment_mode_ids(self):
+        all_payment_mode = self.env['account.payment.mode'].search([])
+        for rec in self:
+            if rec.brand_id and rec.brand_id.allowed_payment_mode_ids:
+                rec.allowed_payment_mode_ids = (
+                    rec.brand_id.allowed_payment_mode_ids
+                )
+            else:
+                rec.allowed_payment_mode_ids = all_payment_mode
