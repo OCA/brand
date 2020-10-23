8876af593b845f8eaf8931eb6d8a9679ad8d9399
sbejaoui <souheil.bejaoui@acsone.eu>
2020-10-24 00:34:12 +0200

    [MIG] - migration analytic_brand 13.0

--- analytic_brand/__manifest__.py
+++ analytic_brand/__manifest__.py
@@ -2,17 +2,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Analytic Brand',
-    'summary': """
+    "name": "Analytic Brand",
+    "summary": """
         This addon associate an analytic account to a brand that will be used
         as a default value where the brand is used if the analytic accounting
         is activated""",
-    'version': '12.0.1.0.1',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/brand',
-    'depends': ['analytic', 'brand'],
-    'data': ['views/res_brand.xml'],
-    'demo': [],
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/brand",
+    "depends": ["analytic", "brand"],
+    "data": ["views/res_brand.xml"],
+    "demo": [],
+    "maintainers": ["sbejaoui"],
 }
