7ae1ad842a927020ee569407ede4f20dfcbfe355
sbejaoui <souheil.bejaoui@acsone.eu>
2020-10-24 20:03:30 +0200

    [13.0][MIG] - migration contract_brand

--- contract_brand/__manifest__.py
+++ contract_brand/__manifest__.py
@@ -7,11 +7,11 @@
         This module allows you to manage branded contracts.
         It adds a brand field on the contract and propagate the value on the
         invoices.""",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
-    "author": "ACSONE SA/NV,"
-              "Odoo Community Association (OCA)",
+    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/contract",
     "depends": ["contract", "account_brand", "analytic_brand"],
     "data": ["views/contract.xml"],
+    "maintainers": ["sbejaoui"],
 }
--- contract_brand/tests/test_contract.py
+++ contract_brand/tests/test_contract.py
@@ -9,45 +9,24 @@ class TestContract(TestContractBase):
         super(TestContract, self).setUp()
         self.brand_id = self.env["res.brand"].create({"name": "brand"})
         self.account_receivable_type = self.env.ref(
-            'account.data_account_type_receivable'
-        )
-        self.account_receivable_brand_default = self.env[
-            'account.account'
-        ].create(
-            {
-                'name': 'Receivable Brand Default',
-                'code': 'RCV01',
-                'user_type_id': self.account_receivable_type.id,
-                'reconcile': True,
-            }
-        )
-        self.partner_account_brand = self.env['res.partner.account.brand'].create(
-            {
-                'partner_id': self.contract.partner_id.id,
-                'account_id': self.account_receivable_brand_default.id,
-                'brand_id': self.brand_id.id,
-                'account_type': 'receivable',
-            }
+            "account.data_account_type_receivable"
         )
 
-    def test_contract_create_branded_invoice(self):
-        """ It should create a branded invoice based on the contract brand
+    def test_contract_create_branded_move(self):
+        """ It should create a branded move based on the contract brand
         """
         self.contract.brand_id = self.brand_id
-        invoice = self.contract.recurring_create_invoice()
-        self.assertEqual(invoice.brand_id, self.brand_id)
-        self.assertEqual(invoice.account_id, self.account_receivable_brand_default)
+        move = self.contract.recurring_create_invoice()
+        self.assertEqual(move.brand_id, self.brand_id)
 
     def test_contract_analytic_account_onchange_brand(self):
-        self.brand_id.analytic_account_id = self.env[
-            'account.analytic.account'
-        ].create({'name': 'analytic account'})
-        self.contract.brand_id = self.brand_id
-        self.assertFalse(
-            self.contract.contract_line_ids.mapped('analytic_account_id')
+        self.brand_id.analytic_account_id = self.env["account.analytic.account"].create(
+            {"name": "analytic account"}
         )
+        self.contract.brand_id = self.brand_id
+        self.assertFalse(self.contract.contract_line_ids.mapped("analytic_account_id"))
         self.contract._onchange_brand_id()
         self.assertEqual(
-            self.contract.contract_line_ids.mapped('analytic_account_id'),
+            self.contract.contract_line_ids.mapped("analytic_account_id"),
             self.brand_id.analytic_account_id,
         )
