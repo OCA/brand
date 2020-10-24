395425da0c19ea1ddbc58a2be3f52a8eb8adea45
sbejaoui <souheil.bejaoui@acsone.eu>
2020-10-24 20:03:30 +0200

    [13.0][MIG] - migration contract_brand

--- contract_brand/models/contract.py
+++ contract_brand/models/contract.py
@@ -6,27 +6,22 @@ from odoo import api, models
 
 class ContractContract(models.Model):
 
-    _name = 'contract.contract'
-    _inherit = ['contract.contract', 'res.brand.mixin']
+    _name = "contract.contract"
+    _inherit = ["contract.contract", "res.brand.mixin"]
 
-    @api.multi
     def _prepare_invoice(self, date_invoice, journal=None):
         self.ensure_one()
-        values = super()._prepare_invoice(date_invoice, journal)
+        values, move_form = super()._prepare_invoice(date_invoice, journal)
         values["brand_id"] = self.brand_id.id
-        # We remove account_id in order to let account_invoice choose
-        # the right account_id (according to the brand)
-        if 'account_id' in values:
-            values.pop('account_id')
-        return values
+        return values, move_form
 
-    @api.onchange('brand_id', 'contract_line_ids')
+    @api.onchange("brand_id", "contract_line_ids")
     def _onchange_brand_id(self):
         res = super()._onchange_brand_id()
         for contract in self:
             if contract.brand_id:
                 analytic_account = contract.brand_id.analytic_account_id
                 contract.contract_line_ids.update(
-                    {'analytic_account_id': analytic_account.id}
+                    {"analytic_account_id": analytic_account.id}
                 )
         return res
