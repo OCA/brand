e79fbe90f777225d84fb44e4d7c4c03fcbb9c64e
Don Kendall <kendall@donkendall.com>
2020-11-27 12:08:48 -0500

    [IMP] ALL(13.0): black, isort, prettier

--- sale_brand/__manifest__.py
+++ sale_brand/__manifest__.py
@@ -4,9 +4,9 @@
 {
     "name": "Sale Brand",
     "summary": "Send branded sales orders",
-    "version": "13.0.0.0.0",
+    "version": "14.0.1.0.0",
     "category": "Sales Management",
-    "website": "https://github.com/OCA/sale-workflow",
+    "website": "https://github.com/OCA/brand",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["sale", "brand", "account_brand"],
