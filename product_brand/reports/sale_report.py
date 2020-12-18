d9c0414d5c6be2ef6e7cfd17fe720d927605a9cb
João Marques <joao.marques@tecnativa.com>
2020-12-18 07:43:05 +0000

    [MIG] product_brand: Migration to 14.0
    Co-authored-by: Bejaoui Souheil <souheil_bejaoui@hotmail.fr>
    
    TT26444

--- product_brand/reports/sale_report.py
+++ product_brand/reports/sale_report.py
@@ -1,4 +1,5 @@
 # Copyright 2018 Tecnativa - David Vidal
+# Copyright 2020 Tecnativa - João Marques
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo import fields, models
@@ -9,10 +10,8 @@ class SaleReport(models.Model):
 
     product_brand_id = fields.Many2one(comodel_name="product.brand", string="Brand")
 
-    # pylint:disable=dangerous-default-value
-    def _query(
-        self, with_clause="", fields={}, groupby="", from_clause=""  # noqa: B006
-    ):
+    def _query(self, with_clause="", fields=None, groupby="", from_clause=""):
+        fields = fields or {}
         fields["product_brand_id"] = ", t.product_brand_id as product_brand_id"
         groupby += ", t.product_brand_id"
-        return super(SaleReport, self)._query(with_clause, fields, groupby, from_clause)
+        return super()._query(with_clause, fields, groupby, from_clause)
