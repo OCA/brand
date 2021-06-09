e8838d2a84e20fe2b2211e31c00e9813ca6997f3
Freni-OSI <fpatel@opensourceintegrators.com>
2021-06-09 12:56:49 +0530

    [14.0][FIX] Travis

--- partner_brand/__manifest__.py
+++ partner_brand/__manifest__.py
@@ -4,11 +4,10 @@
 {
     "name": "Partner Brand",
     "summary": "Manage your brands and use them on your documents",
-    "version": "12.0.2.1.0",
+    "version": "14.0.1.0.0",
     "category": "Sales Management",
     "website": "https://github.com/OCA/brand",
-    "author": "Open Source Integrators,"
-              "Odoo Community Association (OCA)",
+    "author": "Open Source Integrators," "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": [
         "contacts",
