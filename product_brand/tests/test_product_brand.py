d62bca16a9d6d853d4665d17e2ae2b1c091829e0
Simone Orsi <simahawk@gmail.com>
2021-06-22 07:42:23 +0200

    product_brand: add base test class

--- product_brand/tests/test_product_brand.py
+++ product_brand/tests/test_product_brand.py
@@ -1,23 +1,10 @@
 # Copyright (c) 2018 Daniel Campos <danielcampos@avanzosc.es> - Avanzosc S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests import common
+from .common import CommonCase
 
 
-class TestProductBrand(common.TransactionCase):
-    def setUp(self):
-        super().setUp()
-        self.product = self.env.ref("product.product_product_4")
-        self.supplier = self.ref("base.res_partner_2")
-        self.product_brand_obj = self.env["product.brand"]
-        self.product_brand = self.product_brand_obj.create(
-            {
-                "name": "Test Brand",
-                "description": "Test brand description",
-                "partner_id": self.supplier,
-            }
-        )
-
+class TestProductBrand(CommonCase):
     def test_products_count(self):
         self.assertEqual(
             self.product_brand.products_count, 0, "Error product count does not match"
