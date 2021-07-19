b95a54b36b711067a157f13b09688e3501e598f5
Freni-OSI <fpatel@opensourceintegrators.com>
2021-07-19 11:31:03 +0530

    [14.0][FIX] Travis

--- sale_brand/__manifest__.py
+++ sale_brand/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Send branded sales orders",
     "version": "14.0.1.0.0",
     "category": "Sales Management",
-    "website": "https://github.com/OCA/sale-workflow",
+    "website": "https://github.com/OCA/brand",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["sale", "brand", "account_brand", "analytic_brand"],
