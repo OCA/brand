553f8fb76930cb73b8544120e3da2cb041146d3d
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-08-12 11:31:48 -0300

    [IMP] product_brand_tag: Add form view and improve ux

--- product_brand_tag/models/product_brand_tag.py
+++ product_brand_tag/models/product_brand_tag.py
@@ -2,7 +2,7 @@
 # @author: Simone Orsi <simone.orsi@camptocamp.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProductBrandTag(models.Model):
