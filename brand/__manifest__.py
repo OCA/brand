2ce5783a225997a7d761013091b0323474180684
sbejaoui <souheil.bejaoui@acsone.eu>
2022-03-17 14:08:20 +0100

    [MIG] - 15.0 migration

--- brand/__manifest__.py
+++ brand/__manifest__.py
@@ -7,9 +7,9 @@
         This is a base addon for brand modules. It adds the brand object and
         its menu and define an abstract model to be inherited from branded
         objects""",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.1",
     "license": "AGPL-3",
-    "author": "Odoo Community Association (OCA)," "ACSONE SA/NV",
+    "author": "Odoo Community Association (OCA), ACSONE SA/NV",
     "website": "https://github.com/OCA/brand",
     "depends": ["base_setup"],
     "data": [
