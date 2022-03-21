651fdb5a8f1b7363b77c0162571dfbb0240a51b9
sbejaoui <souheil.bejaoui@acsone.eu>
2022-03-21 19:04:18 +0100

    [MIG] - contract_brand migration 15.0

--- contract_brand/__manifest__.py
+++ contract_brand/__manifest__.py
@@ -7,7 +7,7 @@
         This module allows you to manage branded contracts.
         It adds a brand field on the contract and propagate the value on the
         invoices.""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/brand",
