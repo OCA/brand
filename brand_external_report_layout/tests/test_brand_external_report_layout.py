d05cc5fb1d4f6132eac5f2ab90278d6200f78862
sbejaoui <souheil.bejaoui@acsone.eu>
2022-03-21 14:42:05 +0100

    [IMP] - add Brand Document Layout

--- brand_external_report_layout/tests/test_brand_external_report_layout.py
+++ brand_external_report_layout/tests/test_brand_external_report_layout.py
@@ -9,14 +9,5 @@ class TestBrandExternalReportLayout(TransactionCase):
         super(TestBrandExternalReportLayout, self).setUp()
         self.brand = self.env["res.brand"].create({"name": "brand"})
 
-    def test_change_report_template(self):
-        self.brand.change_report_template()
-        self.assertEqual(
-            self.env.ref(
-                "brand_external_report_layout.res_brand_document_template_form"
-            ).id,
-            self.brand.change_report_template()["view_id"],
-        )
-
     def test_get_default_brand_logo(self):
         self.assertEqual(self.brand.logo, self.brand._get_default_brand_logo())
