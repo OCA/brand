f788400d7867781059fc143749f44b3041d2eba5
Eric Antones <eantones@nuobit.com>
2022-12-22 23:34:12 +0100

    [FIX] product_brand: Error getting sale data when pos_sale module is installed
    This solves the issue https://github.com/OCA/brand/issues/141

--- product_brand/reports/sale_report.py
+++ product_brand/reports/sale_report.py
@@ -1,5 +1,6 @@
 # Copyright 2018 Tecnativa - David Vidal
 # Copyright 2020 Tecnativa - João Marques
+# Copyright 2022 NuoBiT - Eric Antones
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo import fields, models
@@ -10,12 +11,10 @@ class SaleReport(models.Model):
 
     product_brand_id = fields.Many2one(comodel_name="product.brand", string="Brand")
 
-    def _select_sale(self):
-        select = super()._select_sale()
-        select = f"""
-            {select},
-            t.product_brand_id as product_brand_id"""
-        return select
+    def _select_additional_fields(self):
+        res = super()._select_additional_fields()
+        res["product_brand_id"] = "t.product_brand_id"
+        return res
 
     def _group_by_sale(self):
         group_by = super()._group_by_sale()
