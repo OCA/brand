f267736576413ff3b8dd72cf16e2d01ce90f3c50
Ilyas <irazor147@gmail.com>
2023-02-01 14:11:36 +0100

    [MIG] product_brand_purchase_report: Migration to 14.0

--- product_brand_purchase_report/__manifest__.py
+++ product_brand_purchase_report/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Brand Purchase Report",
     "summary": """
         This module allows to work with product_brand in purchase reports.""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/brand",
