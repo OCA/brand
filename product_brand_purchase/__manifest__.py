faa2312d3e285c0959a9465fa8dd5cf6887ac924
Ilyas <irazor147@gmail.com>
2023-02-01 14:22:56 +0100

    [UPD] product_brand_purchase_report: rename to product_brand_purchase. Add brand menu into purchase.

--- product_brand_purchase/__manifest__.py
+++ product_brand_purchase/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Product Brand Purchase Report",
+    "name": "Product Brand Purchase",
     "summary": """
         This module allows to work with product_brand in purchase reports.""",
     "version": "14.0.1.0.0",
@@ -10,5 +10,5 @@
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/brand",
     "depends": ["product_brand", "purchase"],
-    "data": ["reports/purchase_report_view.xml"],
+    "data": ["reports/purchase_report_view.xml", "views/product_brand_view.xml"],
 }
