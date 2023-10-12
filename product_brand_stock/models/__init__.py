e420ea2b66c205855bce1f501150d8c2977af18a
Francesco Apruzzese <cescoap@gmail.com>
2023-10-12 12:00:36 +0200

    [ADD][product_brand_stock] New module

---
+++ product_brand_stock/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2023 Francesco Apruzzese <cescoap@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import stock_quant
+from . import stock_move
+from . import stock_move_line
