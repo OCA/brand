b85ab5f47c75f90c2be2bc71f7d4fb4227644612
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-17 12:06:40 +0200

    [IMP] product_brand: test performance improvement - Include context keys for avoiding mail operations overhead.

--- product_brand/tests/common.py
+++ product_brand/tests/common.py
@@ -5,12 +5,14 @@
 
 from odoo.tests.common import TransactionCase
 
+from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+
 
 class CommonCase(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.product = cls.env.ref("product.product_product_4")
         cls.supplier = cls.env.ref("base.res_partner_2")
         cls.product_brand_obj = cls.env["product.brand"]
