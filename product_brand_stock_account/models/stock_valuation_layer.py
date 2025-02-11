9a2f57eb822dad0e9f3859e0cf61741360fa7c59
Bhavesh Heliconia <oca@heliconia.in>
2025-02-11 11:41:16 +0530

    [IMP] product_brand_stock_account: pre-commit auto fixes

--- product_brand_stock_account/models/stock_valuation_layer.py
+++ product_brand_stock_account/models/stock_valuation_layer.py
@@ -5,7 +5,6 @@ from odoo import fields, models
 
 
 class StockValuationLayer(models.Model):
-
     _inherit = "stock.valuation.layer"
 
     product_brand_id = fields.Many2one(
--- product_brand_stock_account/readme/USAGE.rst
+++
@@ -1,1 +0,0 @@
-#. "Brand" group and filter is now available in Stock and Account search view
