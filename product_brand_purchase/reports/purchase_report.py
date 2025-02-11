176e4b33313b4ffdfa8f53077d91deffc0f38eba
Bhavesh Heliconia <oca@heliconia.in>
2025-02-11 09:46:31 +0530

    [IMP] product_brand_purchase: pre-commit auto fixes

--- product_brand_purchase/reports/purchase_report.py
+++ product_brand_purchase/reports/purchase_report.py
@@ -5,15 +5,14 @@ from odoo import fields, models
 
 
 class PurchaseReport(models.Model):
-
     _inherit = "purchase.report"
 
     product_brand_id = fields.Many2one(comodel_name="product.brand", string="Brand")
 
     def _select(self):
-        select_str = super(PurchaseReport, self)._select()
+        select_str = super()._select()
         return select_str + ", t.product_brand_id as product_brand_id"
 
     def _group_by(self):
-        group_by_str = super(PurchaseReport, self)._group_by()
+        group_by_str = super()._group_by()
         return group_by_str + ", t.product_brand_id"
