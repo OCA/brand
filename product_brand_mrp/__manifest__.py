32fda35b95c3d8b4eeadb9783862a1df8d44ab61
Bhavesh Heliconia <oca@heliconia.in>
2025-02-11 10:55:27 +0530

    [MIG] product_brand_mrp: Migration to 18.0

--- product_brand_mrp/__manifest__.py
+++ product_brand_mrp/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Product Brand MRP",
     "summary": """
         This module allows to work with product_brand in MRP.""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/brand",
