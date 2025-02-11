d5652d5753386708905a66ad55dc0324030123a4
Bhavesh Heliconia <oca@heliconia.in>
2025-02-11 10:11:20 +0530

    [MIG] product_brand_tag: Migration to 17.0

--- product_brand_tag/__manifest__.py
+++ product_brand_tag/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Product brand tags",
     "summary": "Add tags to product brand",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Product",
