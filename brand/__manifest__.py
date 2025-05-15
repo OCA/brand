77b21fc465483362dfe0289beeb88dc7492baaab
hda <hughes.damry@acsone.eu>
2025-05-15 08:56:55 +0200

    [18.0][REF] brand: license to LGPL-3

--- brand/__manifest__.py
+++ brand/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2019 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
 
 {
     "name": "Brand",
@@ -8,7 +8,7 @@
         its menu and define an abstract model to be inherited from branded
         objects""",
     "version": "18.0.1.0.0",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "author": "Odoo Community Association (OCA), ACSONE SA/NV",
     "website": "https://github.com/OCA/brand",
     "depends": ["base_setup"],
