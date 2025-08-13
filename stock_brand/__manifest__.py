29dc5f2133be30d8983fe91c44295d4c202a6f6e
Christian-RB <c.ramos@binhex.cloud>
2025-08-13 13:48:24 +0100

    [MIG] stock_brand: Migration to 17.0

--- stock_brand/__manifest__.py
+++ stock_brand/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Brand",
     "summary": "Manage brands on stock picking documents",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Warehouse",
     "website": "https://github.com/OCA/brand",
     "author": "Sunflower IT, Odoo Community Association (OCA)",
