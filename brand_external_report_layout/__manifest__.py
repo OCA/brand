7f89f15a568ee43e85d0062e6cc0a16a6ffe5633
Julien Guenat <julien@jguenat.ch>
2025-10-18 17:03:34 +0200

    [FIX] brand_external_report_layout: asset is required to have brand style in the report

--- brand_external_report_layout/__manifest__.py
+++ brand_external_report_layout/__manifest__.py
@@ -18,4 +18,10 @@
         "wizards/brand_document_layout.xml",
     ],
     "maintainers": ["sbejaoui"],
+    "assets": {
+        "web.report_assets_common": [
+            "/brand_external_report_layout/"
+            "static/src/legacy/scss/asset_styles_brand_report.scss"
+        ]
+    },
 }
