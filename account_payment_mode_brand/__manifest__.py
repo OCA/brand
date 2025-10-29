383df19dd97d62e6b51e708c2e8e5c58f0fcd294
Ruchir Shukla <ruchir@bizzappdev.com>
2025-10-29 16:56:41 +0530

    [IMP] account_payment_mode_brand: pre-commit auto fixes

--- account_payment_mode_brand/__manifest__.py
+++ account_payment_mode_brand/__manifest__.py
@@ -7,7 +7,7 @@
         This addon define allowed payment mode per brand""",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
-    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/brand",
     "depends": [
         "brand",
