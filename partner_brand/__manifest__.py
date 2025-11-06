32a3cf7b29c8b5a1d3eb535208804e521b1e295c
Thomas HERBIN <the@apik.cloud>
2025-11-06 09:25:49 +0100

    [MIG] partner_brand: Migration to 19.0

--- partner_brand/__manifest__.py
+++ partner_brand/__manifest__.py
@@ -1,11 +1,11 @@
 {
     "name": "Partner Brand",
     "summary": "Define registered mark in partners according to brand settings",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.0",
     "author": "Odoo Community Association (OCA), Akretion",
     "development_status": "Alpha",
     "category": "Product",
-    "maintainers": ["bealdav"],
+    "maintainers": ["bealdav", "therbin200"],
     "website": "https://github.com/OCA/brand",
     "license": "AGPL-3",
     "depends": [
