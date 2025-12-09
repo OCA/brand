7c4186b250277c76b0ff389692025e1833529794
Pablo Castelo <pablocv@gmx.es>
2025-12-09 17:05:00 +0100

    [MIG] contract_brand: Migration to 16.0

--- contract_brand/tests/test_contract.py
+++ contract_brand/tests/test_contract.py
@@ -5,11 +5,18 @@ from odoo.addons.contract.tests.test_contract import TestContractBase
 
 
 class TestContract(TestContractBase):
-    def setUp(self):
-        super(TestContract, self).setUp()
-        self.brand_id = self.env["res.brand"].create({"name": "brand"})
-        self.account_receivable_type = self.env.ref(
-            "account.data_account_type_receivable"
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.brand_id = cls.env["res.brand"].create({"name": "brand"})
+        cls.analytic_plan = cls.env["account.analytic.plan"].create(
+            {"name": "analytic plan"}
+        )
+        cls.analytic_account = cls.env["account.analytic.account"].create(
+            {
+                "name": "analytic account",
+                "plan_id": cls.analytic_plan.id,
+            }
         )
 
     def test_contract_create_branded_move(self):
@@ -18,14 +25,13 @@ class TestContract(TestContractBase):
         move = self.contract.recurring_create_invoice()
         self.assertEqual(move.brand_id, self.brand_id)
 
-    def test_contract_analytic_account_onchange_brand(self):
-        self.brand_id.analytic_account_id = self.env["account.analytic.account"].create(
-            {"name": "analytic account"}
+    def test_contract_analytic_distribution_onchange_brand(self):
+        self.brand_id.analytic_distribution = {self.analytic_account.id: 100.0}
+        self.assertFalse(
+            any(self.contract.contract_line_ids.mapped("analytic_distribution"))
         )
         self.contract.brand_id = self.brand_id
-        self.assertFalse(self.contract.contract_line_ids.mapped("analytic_account_id"))
-        self.contract._onchange_brand_id()
-        self.assertEqual(
-            self.contract.contract_line_ids.mapped("analytic_account_id"),
-            self.brand_id.analytic_account_id,
-        )
+        for line in self.contract.contract_line_ids:
+            self.assertEqual(
+                line.analytic_distribution, self.brand_id.analytic_distribution
+            )
