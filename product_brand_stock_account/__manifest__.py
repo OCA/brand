484e104b93da5f4736f488a3004ec5592bccd9b1
emtz10 <eduardoms@vauxoo.com>
2026-02-17 03:32:09 +0000

    [MIG] product_brand_stock_account: Migration to 19.0
    Changes include:
    
    - Add `product_brand_id` field to stock report list view.
    - Deprecated usage of `stock.valuation.layer` model [1].
    
    [1]: https://github.com/odoo/odoo/commit/08b62a4

--- product_brand_stock_account/__manifest__.py
+++ product_brand_stock_account/__manifest__.py
@@ -5,10 +5,10 @@
     "name": "Product Brand Stock Account",
     "summary": """
         This module allows to work with product_brand in Stock Account.""",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/brand",
     "depends": ["product_brand", "stock_account"],
-    "data": ["views/stock_valuation_layer_views.xml"],
+    "data": ["views/product_product_views.xml"],
 }
--- product_brand_stock_account/models/__init__.py
+++ product_brand_stock_account/models/__init__.py
@@ -1,4 +1,2 @@
 # Copyright 2023 Francesco Apruzzese <cescoap@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import stock_valuation_layer
