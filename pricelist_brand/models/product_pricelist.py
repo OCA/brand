8c31e1e98bf131627b52381dd4b17820c52a82fe
AlexGarS73 <alex@studio73.es>
2026-03-24 16:26:03 +0100

    [MIG] pricelist_brand: Migration to 19.0

--- pricelist_brand/models/product_pricelist.py
+++ pricelist_brand/models/product_pricelist.py
@@ -1,16 +1,16 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import api, fields, models
 from odoo.exceptions import ValidationError
-from odoo.osv import expression
+from odoo.fields import Domain
 
 
 class ProductPricelist(models.Model):
     _inherit = "product.pricelist"
 
     def _get_applicable_rules_domain(self, products, date, **kwargs):
-        res = super()._get_applicable_rules_domain(products, date)
+        res = super()._get_applicable_rules_domain(products, date, **kwargs)
         if products._name == "product.template":
             brand_domain = ("product_brand_id", "in", products.product_brand_id.ids)
         else:
@@ -19,7 +19,7 @@ class ProductPricelist(models.Model):
                 "in",
                 products.product_tmpl_id.product_brand_id.ids,
             )
-        res = expression.AND(
+        res = Domain.AND(
             [
                 res,
                 [
@@ -45,38 +45,63 @@ class ProductPricelistItem(models.Model):
     applied_on = fields.Selection(
         selection_add=[("25_brand", "Brand")], ondelete={"25_brand": "set default"}
     )
+    display_applied_on = fields.Selection(
+        selection_add=[("25_brand", "Brand")], ondelete={"25_brand": "set default"}
+    )
+
+    def _is_applicable_for(self, product, qty_in_product_uom):
+        res = super()._is_applicable_for(product, qty_in_product_uom)
+        if not res:
+            return False
+        if self.applied_on == "25_brand":
+            if product._name == "product.template":
+                return product.product_brand_id == self.product_brand_id
+            return product.product_tmpl_id.product_brand_id == self.product_brand_id
+        return res
 
     @api.constrains("product_id", "product_tmpl_id", "categ_id", "product_brand_id")
     def _check_product_consistency(self):
         for item in self:
             if item.applied_on == "25_brand" and not item.product_brand_id:
                 raise ValidationError(
-                    _("Please specify the brand for which this rule should be applied")
+                    self.env._(
+                        "Please specify the brand for which this rule should be applied"
+                    )
                 )
         return super()._check_product_consistency()
 
     @api.depends(
-        "applied_on",
-        "categ_id",
-        "product_tmpl_id",
-        "product_id",
-        "compute_price",
-        "fixed_price",
-        "pricelist_id",
-        "percent_price",
-        "price_discount",
-        "price_surcharge",
+        "applied_on", "categ_id", "product_tmpl_id", "product_id", "product_brand_id"
     )
-    def _compute_name_and_price(self):
-        res = super()._compute_name_and_price()
+    def _compute_name(self):
+        res = super()._compute_name()
         for item in self:
             if item.product_brand_id and item.applied_on == "25_brand":
-                item.name = _("Brand: %s") % (item.product_brand_id.display_name)
+                item.name = self.env._("Brand: %s", item.product_brand_id.display_name)
+        return res
+
+    @api.onchange("display_applied_on")
+    def _onchange_display_applied_on(self):
+        res = super()._onchange_display_applied_on()
+        for item in self:
+            if item.display_applied_on == "25_brand":
+                item.update(
+                    {
+                        "product_id": False,
+                        "product_tmpl_id": False,
+                        "categ_id": False,
+                        "applied_on": "25_brand",
+                    }
+                )
         return res
 
     @api.onchange("product_id", "product_tmpl_id", "categ_id", "product_brand_id")
     def _onchange_rule_content(self):
-        return super()._onchange_rule_content()
+        res = super()._onchange_rule_content()
+        for item in self:
+            if item.product_brand_id:
+                item.applied_on = "25_brand"
+        return res
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -86,16 +111,16 @@ class ProductPricelistItem(models.Model):
                 applied_on = values["applied_on"]
                 if applied_on == "25_brand":
                     values.update(
-                        dict(product_id=None, product_tmpl_id=None, categ_id=None)
+                        dict(product_id=False, product_tmpl_id=False, categ_id=False)
                     )
                 elif applied_on == "3_global":
-                    values.update(dict(product_brand_id=None))
+                    values.update(dict(product_brand_id=False))
                 elif applied_on == "2_product_category":
-                    values.update(dict(product_brand_id=None))
+                    values.update(dict(product_brand_id=False))
                 elif applied_on == "1_product":
-                    values.update(dict(product_brand_id=None))
+                    values.update(dict(product_brand_id=False))
                 elif applied_on == "0_product_variant":
-                    values.update(dict(product_brand_id=None))
+                    values.update(dict(product_brand_id=False))
         return super().create(vals_list)
 
     def write(self, values):
@@ -104,14 +129,14 @@ class ProductPricelistItem(models.Model):
             applied_on = values["applied_on"]
             if applied_on == "25_brand":
                 values.update(
-                    dict(product_id=None, product_tmpl_id=None, categ_id=None)
+                    dict(product_id=False, product_tmpl_id=False, categ_id=False)
                 )
             elif applied_on == "3_global":
-                values.update(dict(product_brand_id=None))
+                values.update(dict(product_brand_id=False))
             elif applied_on == "2_product_category":
-                values.update(dict(product_brand_id=None))
+                values.update(dict(product_brand_id=False))
             elif applied_on == "1_product":
-                values.update(dict(product_brand_id=None))
+                values.update(dict(product_brand_id=False))
             elif applied_on == "0_product_variant":
-                values.update(dict(product_brand_id=None))
+                values.update(dict(product_brand_id=False))
         return super().write(values)
--- pricelist_brand/tests/test_product_pricelist.py
+++ pricelist_brand/tests/test_product_pricelist.py
@@ -1,7 +1,6 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _
 from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
 from odoo.tools import float_compare
@@ -15,11 +14,26 @@ class TestProductPricelist(TransactionCase):
         cls.product_brand = cls.env["product.brand"].create(
             {"name": "Test Brand", "description": "Test brand description"}
         )
-        cls.product = cls.env.ref("product.product_product_4")
-        cls.product.write({"product_brand_id": cls.product_brand.id})
-        cls.product_2 = cls.env.ref("product.product_product_5")
+        cls.product_categ = cls.env["product.category"].create(
+            {"name": "Test Category"}
+        )
+        cls.product = cls.env["product.product"].create(
+            {
+                "name": "Test Product 1",
+                "lst_price": 100.0,
+                "product_brand_id": cls.product_brand.id,
+                "categ_id": cls.product_categ.id,
+            }
+        )
+        cls.product_2 = cls.env["product.product"].create(
+            {
+                "name": "Test Product 2",
+                "lst_price": 200.0,
+                "categ_id": cls.product_categ.id,
+            }
+        )
 
-        cls.list0 = cls.env.ref("product.list0")
+        cls.list0 = cls.env["product.pricelist"].create({"name": "Public Pricelist"})
         cls.pricelist = cls.env["product.pricelist"].create(
             {
                 "name": "Test Pricelist",
@@ -49,7 +63,6 @@ class TestProductPricelist(TransactionCase):
                 ],
             }
         )
-        cls.product_categ = cls.env.ref("product.product_category_2")
 
     def test_ensure_pricelist_item_consistency(self):
         with self.assertRaises(ValidationError):
@@ -71,9 +84,10 @@ class TestProductPricelist(TransactionCase):
             }
         )
         pricelist_item.write({"product_brand_id": self.product_brand.id})
-        pricelist_item._compute_name_and_price()
+        pricelist_item._compute_name()
         self.assertEqual(
-            pricelist_item.name, _("Brand: %s") % (self.product_brand.display_name)
+            pricelist_item.name,
+            self.env._("Brand: %s", self.product_brand.display_name),
         )
         pricelist_item_2 = self.env["product.pricelist.item"].create(
             {
@@ -147,6 +161,31 @@ class TestProductPricelist(TransactionCase):
         pricelist_item.write({"applied_on": "3_global"})
         self.assertFalse(pricelist_item.product_brand_id)
 
+        # Line 117: Create with 3_global
+        pricelist_item_global = self.env["product.pricelist.item"].create(
+            {
+                "pricelist_id": self.pricelist.id,
+                "base": "list_price",
+                "compute_price": "formula",
+                "applied_on": "3_global",
+                "product_brand_id": self.product_brand.id,
+            }
+        )
+        self.assertFalse(pricelist_item_global.product_brand_id)
+
+        # Lines 85-96: _onchange_display_applied_on
+        item_onchange_1 = self.env["product.pricelist.item"].new({})
+        item_onchange_1.display_applied_on = "25_brand"
+        item_onchange_1._onchange_display_applied_on()
+        self.assertEqual(item_onchange_1.applied_on, "25_brand")
+        self.assertFalse(item_onchange_1.product_id)
+
+        # Lines 100-104: _onchange_rule_content
+        item_onchange_2 = self.env["product.pricelist.item"].new({})
+        item_onchange_2.product_brand_id = self.product_brand.id
+        item_onchange_2._onchange_rule_content()
+        self.assertEqual(item_onchange_2.applied_on, "25_brand")
+
     def test_calculation_price_of_products_pricelist(self):
         """Test calculation of product price based on pricelist"""
         # Check sale price of branded product
@@ -177,3 +216,33 @@ class TestProductPricelist(TransactionCase):
             ),
             0,
         )
+
+        # Line 15: Check sale price using product.template instead of product.product
+        template_with_context = self.product.product_tmpl_id.with_context(
+            pricelist=self.pricelist.id, quantity=1
+        )
+        self.assertEqual(
+            float_compare(
+                template_with_context._get_contextual_price(),
+                (
+                    template_with_context.list_price
+                    - template_with_context.list_price * (0.10)
+                ),
+                precision_digits=2,
+            ),
+            0,
+        )
+
+        # Lines 55 and 58: _is_applicable_for
+        brand_item = self.pricelist.item_ids.filtered(
+            lambda x: x.applied_on == "25_brand"
+        )
+
+        # Line 58: test template
+        self.assertTrue(
+            brand_item._is_applicable_for(self.product.product_tmpl_id, 1.0)
+        )
+
+        # Line 55: super() returns False
+        brand_item.min_quantity = 5.0
+        self.assertFalse(brand_item._is_applicable_for(self.product, 1.0))
