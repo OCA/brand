2632a54a8b9151274bb10105ba5825feef9b742b
Bhavesh Heliconia <oca@heliconia.in>
2026-05-18 17:24:56 +0530

    [MIG] sale_brand: Migration to 19.0

--- sale_brand/__manifest__.py
+++ sale_brand/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Brand",
     "summary": "Send branded sales orders",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/brand",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
