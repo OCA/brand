a6f19bbfea8ad1b3e54cfe8f26750bcec7b839ba
Bhavesh Heliconia <oca@heliconia.in>
2026-05-19 17:11:40 +0530

    [MIG] stock_brand: Migration to 19.0

--- stock_brand/__manifest__.py
+++ stock_brand/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Brand",
     "summary": "Manage brands on stock picking documents",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Warehouse",
     "website": "https://github.com/OCA/brand",
     "author": "Sunflower IT, Odoo Community Association (OCA)",
--- stock_brand/readme/CONTRIBUTORS.md
+++ stock_brand/readme/CONTRIBUTORS.md
@@ -1,2 +1,4 @@
 - Terrence Nzaywa \<<terrence@sunflowerweb.nl>,
   <nza.terrence@gmail.com>\>
+- [Heliconia Solutions Pvt. Ltd.](https://www.heliconia.io)
+  - Bhavesh Heliconia
