5dc7e6f4111a84a0ece27c4769ae32108ed662e9
Anmol Garg <anmolgarg12203@gmail.com>
2026-09-11 15:49:45 +0530

    [REF] brand_qweb_pdf_watermark: inherit _get_watermark hook directly

--- brand_qweb_pdf_watermark/models/report.py
+++ brand_qweb_pdf_watermark/models/report.py
@@ -3,35 +3,12 @@
 
 import base64
 
-from odoo import _, api, models
+from odoo import _, models
 from odoo.exceptions import UserError
 
 
 class Report(models.Model):
     _inherit = "ir.actions.report"
-
-    @api.model
-    def _get_report(self, report_ref):
-        report_sudo = super()._get_report(report_ref)
-        if (
-            not report_sudo
-            or report_sudo.pdf_watermark
-            or self.env.context.get("skip_brand_watermark")
-        ):
-            return report_sudo
-
-        docids = self.env.context.get("res_ids")
-        if not docids:
-            return report_sudo
-
-        watermark = self.with_context(skip_brand_watermark=True)._get_watermark(
-            report_sudo, docids=docids
-        )
-        if watermark:
-            b64_watermark = base64.b64encode(watermark)
-            return report_sudo.new({"pdf_watermark": b64_watermark}, origin=report_sudo)
-
-        return report_sudo
 
     def _get_watermark(self, report_ref, docids=False):
         report_sudo = super()._get_report(report_ref)
@@ -52,10 +29,9 @@ class Report(models.Model):
                         )
                         % brand_names
                     )
-                for doc in docs:
-                    if doc.brand_id and doc.brand_id.pdf_watermark:
-                        return base64.b64decode(doc.brand_id.pdf_watermark)
+                if docs:
+                    brand = docs[0].brand_id
+                    if brand and brand.pdf_watermark:
+                        return base64.b64decode(brand.pdf_watermark)
 
-        if hasattr(super(), "_get_watermark"):
-            return super()._get_watermark(report_ref, docids=docids)
-        return None
+        return super()._get_watermark(report_ref, docids=docids)
